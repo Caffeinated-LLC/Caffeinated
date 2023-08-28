ea9d7e695dabce300e2936de005f7ca00a1e3bb9
rania saber <refatsaber@ranias-MacBook-Pro-5.local>
2023-08-28 19:04:12 +0900

    Adding Landing page components

---
+++ src/views/LoginScreen.vue
@@ -0,0 +1,218 @@
+<template>
+  <div :class="$style.LoginScreen">
+    <div :class="$style.registerlogInWrapper">
+      <div :class="$style.registerlogIn">REGISTER/LOG-IN</div>
+    </div>
+    <img :class="$style.caffeinated31" alt="" src="/caffeinated-3-1@2x.png" />
+    <div :class="$style.letsGetCaffeinated">Let’s get caffeinated.</div>
+    <div :class="$style.LoginScreenChild" />
+    <img
+      :class="$style.caffeinated21"
+      alt=""
+      src="/caffeinated-2-1@2x.png"
+      @click="onCaffEINATED21Click"
+    />
+    <img :class="$style.icon" alt="" src="/3-1@2x.png" />
+    <div :class="$style.backButton" @click="onBackButtonContainerClick">
+      <div :class="$style.backButtonChild" />
+      <img :class="$style.backButtonItem" alt="" src="/arrow-1.svg" />
+    </div>
+    <div :class="$style.email">Email</div>
+    <div :class="$style.password">Password</div>
+    <div :class="$style.LoginScreenItem" />
+    <div :class="$style.LoginScreenInner" />
+    <div :class="$style.rectangleDiv" />
+    <div :class="$style.LoginScreenChild1" />
+    <div :class="$style.createUser">Create User</div>
+    <div :class="$style.logIn">Log in</div>
+  </div>
+</template>
+<script>
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "LoginScreen",
+    methods: {
+      onCaffEINATED21Click() {
+        // Please sync "landing-high" to the project
+      },
+      onBackButtonContainerClick() {
+        // Please sync "landing-high" to the project
+      },
+    },
+  });
+</script>
+<style module>
+  .registerlogIn {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    font-weight: 500;
+    display: inline-block;
+    width: 152.38px;
+    height: 85px;
+  }
+  .registerlogInWrapper {
+    position: absolute;
+    top: 359px;
+    left: 551px;
+    width: 152.38px;
+    height: 85px;
+    font-size: 28px;
+    color: var(--color-sienna);
+  }
+  .caffeinated31 {
+    position: absolute;
+    top: 1030px;
+    left: 0px;
+    width: 801px;
+    height: 601px;
+    object-fit: cover;
+  }
+  .letsGetCaffeinated {
+    position: absolute;
+    top: 233px;
+    left: 426px;
+    font-size: 30px;
+    font-weight: 300;
+    display: inline-block;
+    width: 401px;
+    height: 42px;
+  }
+  .LoginScreenChild {
+    position: absolute;
+    top: 1612px;
+    left: 0px;
+    background-color: var(--color-sienna);
+    width: 1254px;
+    height: 122px;
+  }
+  .caffeinated21 {
+    position: absolute;
+    top: 0px;
+    left: 524px;
+    width: 206px;
+    height: 206px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+  .icon {
+    position: absolute;
+    top: 426px;
+    left: 871px;
+    width: 383px;
+    height: 366.67px;
+    object-fit: cover;
+  }
+  .backButtonChild {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: 12px;
+    background-color: var(--color-sienna);
+    width: 114px;
+    height: 49px;
+  }
+  .backButtonItem {
+    position: absolute;
+    top: 10.3px;
+    left: 22.97px;
+    width: 66.14px;
+    height: 29.46px;
+  }
+  .backButton {
+    position: absolute;
+    top: 105px;
+    left: 74px;
+    width: 114px;
+    height: 49px;
+    cursor: pointer;
+  }
+  .email {
+    position: absolute;
+    top: 531px;
+    left: 407px;
+    font-family: var(--font-inter);
+    text-align: left;
+    display: inline-block;
+    width: 68px;
+    height: 26px;
+  }
+  .password {
+    position: absolute;
+    top: 607px;
+    left: 405px;
+    font-family: var(--font-inter);
+    text-align: left;
+    display: inline-block;
+    width: 106px;
+    height: 26px;
+  }
+  .LoginScreenItem {
+    position: absolute;
+    top: 522px;
+    left: 538px;
+    border-radius: var(--br-5xs);
+    background-color: var(--color-white);
+    width: 270px;
+    height: 43px;
+  }
+  .LoginScreenInner {
+    position: absolute;
+    top: 598px;
+    left: 538px;
+    border-radius: var(--br-5xs);
+    background-color: var(--color-white);
+    width: 270px;
+    height: 43px;
+  }
+  .rectangleDiv {
+    position: absolute;
+    top: 765px;
+    left: 565px;
+    border-radius: var(--br-3xs);
+    background-color: #3a3a3a;
+    width: 146px;
+    height: 36px;
+  }
+  .LoginScreenChild1 {
+    position: absolute;
+    top: 831px;
+    left: 551px;
+    border-radius: var(--br-3xs);
+    background-color: var(--color-sienna);
+    width: 174px;
+    height: 36px;
+  }
+  .createUser {
+    position: absolute;
+    top: 769px;
+    left: 579px;
+    font-weight: 500;
+    color: var(--color-white);
+    display: inline-block;
+    width: 117px;
+    height: 27px;
+  }
+  .logIn {
+    position: absolute;
+    top: 833px;
+    left: 579px;
+    font-weight: 500;
+    color: var(--color-white);
+    display: inline-block;
+    width: 117px;
+    height: 27px;
+  }
+  .LoginScreen {
+    position: relative;
+    background-color: #fff4e6;
+    width: 100%;
+    height: 1734px;
+    overflow: hidden;
+    text-align: center;
+    font-size: var(--font-size-xl);
+    color: var(--color-black);
+    font-family: var(--font-jost);
+  }
+</style>
---
+++ src/views/MapResults.vue
@@ -0,0 +1,50 @@
+<div class="CaffeinatedMapAndResults" style="width: 1440px; height: 982px; position: relative; background: #BABABA">
+    <div class="Rectangle22" style="width: 276px; height: 59px; left: 292px; top: 138px; position: absolute; background: #8A8181; border-radius: 5px"></div>
+    <div class="Rectangle21" style="width: 271px; height: 59px; left: 23px; top: 138px; position: absolute; background: #D9D9D9; border-radius: 5px"></div>
+    <div class="Footer" style="width: 1440px; height: 73px; left: 0px; top: 909px; position: absolute">
+      <div class="Footer" style="width: 1440px; height: 81px; left: 0px; top: -8px; position: absolute; background: #CDCCCC; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 10px"></div>
+      <div class="FooterStuff" style="width: 328px; height: 53px; left: 556px; top: 10px; position: absolute; text-align: center; color: black; font-size: 36px; font-family: Inter; font-weight: 400; word-wrap: break-word">Footer stuff</div>
+    </div>
+    <div class="Matches" style="width: 545px; height: 657px; left: 23px; top: 225px; position: absolute; flex-direction: column; justify-content: center; align-items: center; display: inline-flex">
+      <div class="Group6" style="width: 545px; height: 657px; position: relative">
+        <div class="Rectangle3" style="width: 545px; height: 657px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 10px"></div>
+        <div class="Match3" style="width: 491.24px; height: 181.14px; left: 31.23px; top: 30.03px; position: absolute">
+          <div class="Rectangle4" style="width: 491.24px; height: 181.14px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 10px"></div>
+          <div class="RestaurantTitleYelpRatingGMapsRatingHours" style="width: 174.43px; height: 57.25px; left: 192.77px; top: 15.97px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">RESTAURANT TITLE<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">YELP RATING & G-MAPS RATING<br/>$$$<br/>HOURS</span></div>
+          <div class="MatchFilter1Filter2Filter3Filter4" style="width: 174.43px; height: 57.25px; left: 192.77px; top: 107.97px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">80% MATCH<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">FILTER 1, FILTER 2, FILTER 3. FILTER 4</span></div>
+          <img class="Image1" style="width: 177.07px; height: 149.23px; left: 6.83px; top: 15.96px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/177x149" />
+        </div>
+        <div class="Match2" style="width: 495.87px; height: 181.14px; left: 25.18px; top: 444.88px; position: absolute">
+          <div class="Rectangle4" style="width: 495.87px; height: 181.14px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 10px"></div>
+          <div class="RestaurantTitleYelpRatingGMapsRatingHours" style="width: 157.09px; height: 57.25px; left: 175.82px; top: 18.12px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">RESTAURANT TITLE<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">YELP RATING & G-MAPS RATING<br/>$$$<br/>HOURS</span></div>
+          <div class="MatchFilter1Filter2Filter3Filter4" style="width: 157.09px; height: 57.25px; left: 175.82px; top: 108.12px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">80% MATCH<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">FILTER 1, FILTER 2, FILTER 3. FILTER 4</span></div>
+          <img class="Image1" style="width: 159.47px; height: 149.23px; left: 6.35px; top: 15.96px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/159x149" />
+        </div>
+        <div class="Match1" style="width: 495.87px; height: 181.14px; left: 25.18px; top: 237.46px; position: absolute">
+          <div class="Rectangle4" style="width: 495.87px; height: 181.14px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 10px"></div>
+          <div class="RestaurantTitleYelpRatingGMapsRatingHours" style="width: 157.09px; height: 57.25px; left: 189.82px; top: 18.54px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">RESTAURANT TITLE<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">YELP RATING & G-MAPS RATING<br/>$$$<br/>HOURS</span></div>
+          <div class="MatchFilter1Filter2Filter3Filter4" style="width: 157.09px; height: 57.25px; left: 189.82px; top: 108.54px; position: absolute"><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 700; word-wrap: break-word">80% MATCH<br/></span><span style="color: black; font-size: 12px; font-family: Inter; font-weight: 400; word-wrap: break-word">FILTER 1, FILTER 2, FILTER 3. FILTER 4</span></div>
+          <img class="Image1" style="width: 159.47px; height: 149.23px; left: 6.35px; top: 15.96px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/159x149" />
+        </div>
+      </div>
+    </div>
+    <div class="Map" style="width: 817px; height: 758px; left: 606px; top: 136px; position: absolute">
+      <div class="Rectangle2" style="width: 817px; height: 758px; left: 0px; top: 0px; position: absolute; background: #272727; border-radius: 10px"></div>
+      <img class="Mapla1" style="width: 722.54px; height: 619.39px; left: 47.84px; top: 69.30px; position: absolute; border-radius: 10px" src="https://via.placeholder.com/723x619" />
+      <div class="Rectangle27" style="width: 434px; height: 45px; left: 170.50px; top: 9px; position: absolute; background: #D9D9D9; border-radius: 10px"></div>
+      <div class="InterfaceEssentialMagnifier" style="width: 46px; height: 46px; padding-top: 3.83px; padding-bottom: 4.31px; padding-left: 5.75px; padding-right: 4.22px; left: 171px; top: 8px; position: absolute; justify-content: center; align-items: center; display: inline-flex">
+        <div class="Icon" style="width: 36.03px; height: 37.85px; background: #141010"></div>
+      </div>
+      <div class="LosAngelesCa" style="width: 217px; height: 27px; left: 272px; top: 18px; position: absolute; text-align: center; color: #131212; font-size: 25px; font-family: Inter; font-weight: 700; word-wrap: break-word">Los Angeles, CA</div>
+    </div>
+    <div class="Line1" style="width: 59.01px; height: 0px; left: 294px; top: 197.01px; position: absolute; transform: rotate(-90deg); transform-origin: 0 0; border: 1px black solid"></div>
+    <div class="Filters" style="width: 85px; height: 47px; left: 108px; top: 150px; position: absolute; color: black; font-size: 25px; font-family: Inter; font-weight: 400; word-wrap: break-word">Filters</div>
+    <div class="Results" style="width: 91px; height: 30px; left: 376px; top: 150px; position: absolute; color: black; font-size: 25px; font-family: Inter; font-weight: 400; word-wrap: break-word">Results</div>
+    <div class="Header" style="width: 1440px; height: 122px; left: 0px; top: 0px; position: absolute">
+      <div class="HeaderRectangle" style="width: 1440px; height: 122px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 1px"></div>
+      <div class="AboutUs" style="width: 83px; height: 24px; left: 945px; top: 55px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Inter; font-weight: 400; word-wrap: break-word">About us</div>
+      <div class="Rectangle17" style="width: 135px; height: 81px; left: 652px; top: 21px; position: absolute; background: #8F8F8F; border-radius: 23px"></div>
+      <div class="Logo" style="width: 76px; height: 36px; left: 682px; top: 43px; position: absolute; text-align: center; color: white; font-size: 25px; font-family: Inter; font-weight: 400; word-wrap: break-word">logo</div>
+      <div class="Locations" style="width: 83px; height: 24px; left: 411px; top: 55px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Inter; font-weight: 400; word-wrap: break-word">Locations</div>
+    </div>
+  </div>
