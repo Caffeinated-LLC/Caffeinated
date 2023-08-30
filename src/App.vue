fd4c707c7b93065ad4412b49dbd37978a1f28422
naridad <naridad+gitberk@protonmail.com>
2023-08-30 14:21:48 +0900

    halfway through
    changing submit a review to add a cafe. adding new cafes, updating existing cafe information, searching by city and my certain amenities

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/db">Submit a Review</RouterLink>
+        <RouterLink to="/db">Add a Cafe</RouterLink>
       </nav>
   </header>
 
--- src/views/DbView.vue
+++ src/views/DbView.vue
@@ -1,46 +1,57 @@
 <template>
     <div class="db">
-      <h1>Submit a Review</h1>
+      <h1>Add a Cafe</h1>
   
-      <!-- Section for Review Creation & Update -->
-      Email:<input type="text" v-model.trim="email" />
+      <!-- Section for Cafe Creation & Update -->
+      Cafe ID (Update):<input type="text" v-model.trim="cafeUpdateId" />
       <br>
       City:<input type="text" v-model.trim="city" />
       <br>
-      Cafe Name:<input type="text" v-model.trim="cafeName" />
+      Cafe Name:<input type="text" v-model.trim="name" />
       <br>
-      Rating:<input type="number" min="1" max="5" v-model.trim="reviewScore" />
-      <br>
-      Review Content:<input type="text" v-model.trim="reviewContent" />
-      <br>
-      <button @click="createReview()">Create</button>
+      <button v-if="cafeUpdateId != null && cafeUpdateId.length > 0" @click="updateCafe()">Update an Existing Cafe</button>
+      <button v-else @click="createCafe()">Add a New Cafe</button>
       <br>
   
-      <!-- Section for Review Query -->
+      <!-- Section for Cafe Query -->
       Filter by City:<input type="text" v-model.trim="cityFilter" />
       <br>
-      <button @click="queryReviews()">Query</button>
-      <template v-for="review in reviews">
-        <p>{{ review.city || 'Missing City' }} {{ review.reviewScore || 'Missing Rating' }} 
+      <button @click="queryCity()">Query</button>
+      <template v-for="Cafe in Cafes">
+        <p>Cafe Name: {{ Cafe.name || 'Missing Cafe Name' }}
           <br>
-          {{ review.reviewContent || 'No Review Included' }}
+          City: {{ Cafe.city || 'Missing City' }}
+        </p>
+      </template>
+      <br>
+
+      <!-- Section for Cafe Query -->
+      Wifi Available:<input type="text" v-model.trim="wifiFilter" />
+      <br>
+      <button @click="queryWifi()">Query</button>
+      <template v-for="Cafe in Cafes">
+        <p>Cafe Name: {{ Cafe.name || 'Missing Cafe Name' }}
+          <br>
+          City: {{ Cafe.city || 'Missing City' }}
+          <br>
+          Wifi: {{ Cafe.city || 'Missing Wifi Availability' }}
         </p>
       </template>
       <br>
   
-      <!-- Section for Review Read -->
-      ID Lookup:<input type="text" v-model.trim="reviewReadId" />
+      <!-- Section for Cafe Read -->
+      ID Lookup:<input type="text" v-model.trim="CafeReadId" />
       <br>
-      <button @click="readReview()">Read</button>
-      <template v-if="readReviewsData">
-        <p>{{ readReviewsData.email || 'Missing Email' }} {{ readReviewsData.city || 'Missing City' }} - {{ readReviewsData.id }}</p>
+      <button @click="readCafe()">Read</button>
+      <template v-if="readCafesData">
+        <p>{{ readCafesData.city || 'Missing City' }} - {{ readCafesData.id }}</p>
       </template>
       <br>
   
-      <!-- Section for Review Delete -->
-      ID Delete:<input type="text" v-model.trim="reviewDeleteId" />
+      <!-- Section for Cafe Delete -->
+      ID Delete:<input type="text" v-model.trim="CafeDeleteId" />
       <br>
-      <button @click="deleteReview()">Delete</button>
+      <button @click="deleteCafe()">Delete</button>
     </div>
   </template>
   
@@ -62,41 +73,42 @@
     data() {
       return {
         // data for display
-        reviews: [],
-        readReviewsData: null,
+        Cafes: [],
+        readCafesData: null,
   
         // v-model variable for update
-        reviewUpdateId: null,
+        CafeUpdateId: null,
   
         // v-model variables for creation
+        cafeUpdateId: null,
         city: null,
-        email: null,
-        reviewScore: null,
-        reviewContent: null,
-        cafeName: null,
+        name: null,
   
         // v-model variable for query
         cityFilter: null,
+
+        // v-model variable for query
+        wifiFilter: null,
   
         // v-model variable for read
-        reviewReadId: null,
+        CafeReadId: null,
   
         // v-model variable for delete
-        reviewDeleteId: null,
+        CafeDeleteId: null,
       };
     },
     methods: {
-      async createReview() {
+      async createCafe() {
         if (
-          this.email != null &&
-          this.email.length > 0 &&
           this.city != null &&
-          this.city.length > 0
+          this.city.length > 0 &&
+          this.name != null &&
+          this.name.length > 0
         ) {
           try {
-            console.log('Calling createReview');
+            console.log('Calling createCafe');
   
-            console.log('Creating review:', { email: this.email, city: this.city, reviewScore: this.reviewScore, reviewContent: this.reviewContent, cafeName: this.cafeName});
+            console.log('Creating Cafe:', { city: this.city, name: this.name});
   
             // We use the addDoc function to insert a document into the database. We start with the
             // collection that we want to insert into (in this case eployees) followed by an object
@@ -106,38 +118,89 @@
             // recommend in many cases) then you can create a converter function that returns an
             // object representing how the class should be written to the database
             const docReference = await addDoc(
-              collection(db, 'reviews'),
+              collection(db, 'cafes'),
               {
-                email: this.email,
                 city: this.city,
-                cafeName: this.cafeName,
-                reviewScore: this.reviewScore,
-                reviewContent: this.reviewContent,
+                name: this.name,
               }
             );
   
             // We could specify the id of the new document were inserting by adding it to the end of
-            // the collection like 'collection(db, 'reviews', someId)' but if we omit that then it
+            // the collection like 'collection(db, 'cafes', someId)' but if we omit that then it
             // will have a random id generated automatically
-            console.log('New review has ID:', docReference.id);
+            console.log('New Cafe has ID:', docReference.id);
   
-            console.log('Completed createReview')
+            console.log('Completed createCafe')
           } catch(err) {
-            console.error('Error in createReview', err);
+            console.error('Error in createCafe', err);
           }
         }
       },
-      async queryReviews() {
+    async updateCafe() {
+      if (this.cafeUpdateId != null &&
+        this.cafeDeleteId.length > 0 &&
+        this.name != null &&
+        this.name.length > 0
+      ) {
         try {
-          console.log('Calling queryReviews');
+          console.log('Calling updateCafe')
+
+          console.log('Updating cafe:', {
+            id: this.cafeUpdateId,
+            city: this.city,
+            name: this.name
+          });
+          await setDoc(
+            doc(db, 'cafes', this.cafeUpdateId),
+            {
+              city: this.city,
+              name: this.name,
+            },
+            // { merge: true }
+          );
+
+          console.log('Completed updateCafe')
+        } catch(err) {
+          console.error('Error in updateCafe', err);
+        }
+      }
+    },
+    async queryWifi() {
+        try {
+          console.log('Calling queryWifi');
+          this.Cafes = [];
+          let q = query(collection(db, 'cafes'));
+          if (this.cityFilter) {
+            console.log('Adding filter', this.cityFilter);
+            q = query(q, where('wifi', '==', this.cityFilter));
+          }
+          const queryResponse = await getDocs(q);
   
-          // Reset the reviews data, typically you would hide data loads
+          // Iterate over the results to add them to the Cafes reactive variable
+          queryResponse.forEach((responseItem) => {
+            console.log({ id: responseItem.id, data: responseItem.data() });
+              this.Cafes.push({
+              id: responseItem.id,
+              ...responseItem.data(),
+            });
+          });
+  
+          console.log('Completed queryWifi');
+        } catch(err) {
+          console.error('Error in queryWifi', err);
+        }
+      },
+      async queryCity() {
+        try {
+          console.log('Calling queryCity');
+  
+          // Reset the Cafes data, typically you would hide data loads
           // behind a loader which you would turn on when this operation starts
           // and turn off when the data is loaded
-          this.reviews = [];
+          this.Cafes = [];
   
-          // Create a query against the reviews collection
-          let q = query(collection(db, 'reviews'));
+          // Create a query against the Cafes collection
+          let q = query(collection(db, 'cafes'));
   
           // If the cityFilter input has some value (is not falsy) then we
           // apply a where filter to check for matching first names. Note that
@@ -152,33 +215,33 @@
           // get a single record
           const queryResponse = await getDocs(q);
   
-          // Iterate over the results to add them to the reviews reactive variable
+          // Iterate over the results to add them to the Cafes reactive variable
           queryResponse.forEach((responseItem) => {
             console.log({ id: responseItem.id, data: responseItem.data() });
   
-            // Note that here we add objects to the reviews reactive variable, and
+            // Note that here we add objects to the Cafes reactive variable, and
             // that object includes the id of the record as well as spreading all the
             // fields the record itself contains. This allows us to reference each field
             // in the document directly (as well as the id if needed)
-            this.reviews.push({
+            this.Cafes.push({
               id: responseItem.id,
               ...responseItem.data(),
             });
           });
   
-          console.log('Completed queryReviews');
+          console.log('Completed queryCity');
         } catch(err) {
-          console.error('Error in queryReviews', err);
+          console.error('Error in queryCity', err);
         }
       },
-      async readReview() {
-        if (this.reviewReadId != null && this.reviewReadId.length > 0) {
+      async readCafe() {
+        if (this.CafeReadId != null && this.CafeReadId.length > 0) {
           try {
-            console.log('Calling readReview');
+            console.log('Calling readCafe');
   
-            // Create a doc reference with the reviews collection and the
+            // Create a doc reference with the Cafes collection and the
             // id of the record we want to retrieve
-            const docReference = doc(db, 'reviews', this.reviewReadId);
+            const docReference = doc(db, 'cafes', this.CafeReadId);
   
             // Call getDoc to retrieve the specific document that we want
             // This represents a specific get rather than a query and is
@@ -192,33 +255,33 @@
             // to use search results. You could even create a class or function
             // that takes in the data and generates this type of object and
             // use that everywhere for consistency
-            this.readReviewsData = {
+            this.readCafesData = {
               id: response.id,
               ...response.data(),
             }
-            // We could also have re-used the reviews reactive variable and
+            // We could also have re-used the Cafes reactive variable and
             // replaced it with this data rather than using a separate variable
             // and elements
   
-            console.log('Completed readReviews');
+            console.log('Completed readCafes');
           } catch(err) {
-            console.error('Error in readReview', err);
+            console.error('Error in readCafe', err);
           }
         }
       },
-      async deleteReview() {
-        if (this.reviewDeleteId != null && this.reviewDeleteId.length > 0) {
+      async deleteCafe() {
+        if (this.CafeDeleteId != null && this.CafeDeleteId.length > 0) {
           try {
-            console.log('Calling deleteReview');
+            console.log('Calling deleteCafe');
   
-            const docReference = doc(db, 'reviews', this.reviewDeleteId);
+            const docReference = doc(db, 'cafes', this.CafeDeleteId);
             // Note that deleting a document that doesn't exist doesn't throw
             // and error and the deleteDoc function returns nothing (they say
             // if you delete something that doens't exist then it was successful
             // which sorta makes sense)
             await deleteDoc(docReference);
             // If we wanted to be reactive we could do any of the following:
-            // 1. Lookup the deleted id in reviews array and if it exists
+            // 1. Lookup the deleted id in Cafes array and if it exists
             //    then remove it. This is very quick, but you might need to
             //    add it back in the case that the delete actually fails
             // 2. Trigger the query function again after the delete completes
@@ -230,9 +293,9 @@
             // snapshot listener which can be given a callback that will be
             // triggered each time the document is updated
   
-            console.log('Completed deleteReview');
+            console.log('Completed deleteCafe');
           } catch(err) {
-            console.error('Error in deleteReview', err);
+            console.error('Error in deleteCafe', err);
           }
         }
       },
